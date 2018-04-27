b4afe3a5140256fa952c6b203f443883410f2f4f
SC <sunweichao@rayclear.com>
2018-04-27 10:13:46 +0800

    Initial Commit

---
+++ Live/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Live
+//
+//  Created by SC on 2018/4/27.
+//  Copyright © 2018年 SC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
